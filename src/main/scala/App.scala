37d87516653d2eabac6e2274d4a62c009cea74fd
Jaspreet S Sethi <jaspreet.s.sethi@live.com>
2015-07-29 23:05:57 -0500

    initial setup for graphics setup

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,6 +1,34 @@
 import org.lwjgl.Sys
+import org.lwjgl.glfw.GLFW._
+import org.lwjgl.glfw.{GLFWKeyCallback, Callbacks}
+import org.lwjgl.opengl.GLContext
+import org.lwjgl.opengl.GL11._
 
 object Main extends App {
   println("Hello basic-project!")
   println(s"LWJGL Version ${Sys.getVersion} is working.")
+
+  glfwSetErrorCallback(Callbacks.errorCallbackPrint())
+  if(glfwInit() != GL_TRUE){
+    throw new IllegalStateException("Unable to initialize GLFW")
+  }
+
+  val window = glfwCreateWindow(640, 480, "Simple example", 0, 0);
+  if (window == 0) {
+    glfwTerminate();
+    throw new RuntimeException("Failed to create the GLFW window");
+  }
+
+  glfwMakeContextCurrent(window)
+  GLContext.createFromCurrent()
+
+  glfwSetKeyCallback(window, new GLFWKeyCallback {
+    override def invoke(winsow: Long, key: Int, i1: Int, action: Int, i3: Int): Unit = if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) glfwSetWindowShouldClose(window, GL_TRUE)
+  })
+
+  while (glfwWindowShouldClose(window) != GL_TRUE) {
+    val time = glfwGetTime()
+    glfwSwapBuffers(window)
+    glfwPollEvents()
+  }
 }
